23f2079931dd3208f5c27bb839f219c1e8ae74fc
Reelyx <reelynotfound@gmail.com>
2025-01-22 23:20:34 +0800

    Add files via upload

--- Forms.py
+++ Forms.py
@@ -1,24 +1,11 @@
-from wtforms import Form, StringField, RadioField, SelectField, TextAreaField, validators, IntegerField
+from wtforms import Form, StringField, RadioField, SelectField, TextAreaField, validators
 from wtforms.fields import EmailField, DateField
+from wtforms.validators import DataRequired
 
 class CreateUserForm(Form):
-    first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    last_name = StringField('Last Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    gender = SelectField('Gender', [validators.DataRequired()], choices=[('', 'Select'), ('F', 'Female'), ('M', 'Male')], default='')
-    membership = RadioField('Membership', choices=[('F', 'Fellow'), ('S', 'Senior'), ('P', 'Professional')], default='F')
-    remarks = TextAreaField('Remarks', [validators.Optional()])
+    title = StringField('Title', [validators.Length(min=1, max=50), validators.DataRequired()])
+    name = StringField('Name', [validators.Length(min=1, max=300), validators.DataRequired()])
+    amount_paid = StringField('Amount Paid', [validators.DataRequired()])
+    refund_status = RadioField('Refund Status', choices=[('Not Refunded', 'Not Refunded'), ('Refunded', 'Refunded')], validators=[DataRequired()])
 
-class CreateCustomerForm(Form):
-    first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    last_name = StringField('Last Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    gender = SelectField('Gender', [validators.DataRequired()], choices=[('', 'Select'), ('F', 'Female'), ('M', 'Male')], default='')
-    email = EmailField('Email', [validators.Email(), validators.DataRequired()])
-    date_joined = DateField('Date Joined', format='%Y-%m-%d')
-    address = TextAreaField('Mailing Address', [validators.length(max=200), validators.DataRequired()])
-    membership = RadioField('Membership', choices=[('F', 'Fellow'), ('S', 'Senior'), ('P', 'Professional')], default='F')
-    remarks = TextAreaField('Remarks', [validators.Optional()])
-class CreateEbookForm(Form):
-    title = StringField('Title', [validators.DataRequired(), validators.Length(min=1, max=200)])
-    description = TextAreaField('Description', [validators.Optional(), validators.Length(max=500)])
-    author = StringField('Author', [validators.DataRequired(), validators.Length(min=1, max=100)])
-    genre = StringField('Genre', [validators.DataRequired(), validators.Length(min=1, max=100)])
+
