79f47284eaf7df8af5a6f398d72335d888b08d8e
Rampqge <89903590+Rampqge@users.noreply.github.com>
2025-01-22 19:53:27 +0800

    Forms with ebooks

--- Forms.py
+++ Forms.py
@@ -1,22 +1,24 @@
 from wtforms import Form, StringField, RadioField, SelectField, TextAreaField, validators, IntegerField
 from wtforms.fields import EmailField, DateField
 
-class CreateCardForm(Form):
+class CreateUserForm(Form):
     first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
     last_name = StringField('Last Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    card_number = StringField('Card_Number', [validators.DataRequired(), validators.Length(min=16, max=16)])
-    expiry_date = DateField('Expiry Date', [validators.DataRequired()], format='%Y-%d-%m')
-    cvc_number = StringField('CVC', [validators.DataRequired(), validators.Length(min=3, max=3)])
+    gender = SelectField('Gender', [validators.DataRequired()], choices=[('', 'Select'), ('F', 'Female'), ('M', 'Male')], default='')
+    membership = RadioField('Membership', choices=[('F', 'Fellow'), ('S', 'Senior'), ('P', 'Professional')], default='F')
+    remarks = TextAreaField('Remarks', [validators.Optional()])
 
 class CreateCustomerForm(Form):
     first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
     last_name = StringField('Last Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    gender = SelectField('Gender', [validators.DataRequired()],
-                         choices=[('', 'Select'), ('F', 'Female'), ('M', 'Male')],
-                         default='')
+    gender = SelectField('Gender', [validators.DataRequired()], choices=[('', 'Select'), ('F', 'Female'), ('M', 'Male')], default='')
     email = EmailField('Email', [validators.Email(), validators.DataRequired()])
     date_joined = DateField('Date Joined', format='%Y-%m-%d')
     address = TextAreaField('Mailing Address', [validators.length(max=200), validators.DataRequired()])
-    membership = RadioField('Membership', choices=[('F', 'Fellow'), ('S', 'Senior'), ('P', 'Professional')],
-                            default='F')
+    membership = RadioField('Membership', choices=[('F', 'Fellow'), ('S', 'Senior'), ('P', 'Professional')], default='F')
     remarks = TextAreaField('Remarks', [validators.Optional()])
+class CreateEbookForm(Form):
+    title = StringField('Title', [validators.DataRequired(), validators.Length(min=1, max=200)])
+    description = TextAreaField('Description', [validators.Optional(), validators.Length(max=500)])
+    author = StringField('Author', [validators.DataRequired(), validators.Length(min=1, max=100)])
+    genre = StringField('Genre', [validators.DataRequired(), validators.Length(min=1, max=100)])
